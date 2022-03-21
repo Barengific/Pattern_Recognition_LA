45292c8f9f38e555a2acdde59e1275c998f3c7d6
Barengific <75044077+Barengific@users.noreply.github.com>
2022-03-21 13:56:57 +0000

    Update Batch_Perceptron_LA_LD.py

--- Batch_Perceptron_LA_LD.py
+++ Batch_Perceptron_LA_LD.py
@@ -2,8 +2,10 @@
 # linear discriminant function, dichotomizer,
 # using batch perceptron learning algorithm
 # with augmented notation and sample normalisation
-# data set
+# learning is done after each epoch
+# 
 
+# data set
 #  x   class
 # 1,5    1
 # 2,5    1
@@ -33,6 +35,7 @@ y = np.array([[1,1,-1,-1],
 # misclassified if, g(x) <= 0
 
 #epoch 1
+print("___Epoch1___") 
 r,c = y.shape
 
 g = ''
@@ -47,5 +50,28 @@ _r,_c = _x.shape
 for i in range(1,_c):    
     a = np.add(a, _x[:,i])
     
+a = lr * a
+print()    
+print("new a: ", a)
+
+
+
+#epoch 2
+print("___Epoch2___") 
+r,c = y.shape
+
+g = ''
+_x = np.array([[0],[0],[0]])
+for i in range(0,c):
+    g = np.dot(a.transpose(),y[:,i])
+    print("***g: ", g)
+    if g <= 0:
+        _x = np.insert(_x, 1, y[:,i], axis=1)
+
+_r,_c = _x.shape
+for i in range(1,_c):    
+    a = np.add(a, _x[:,i])
+    
+a = lr * a
 print()    
 print("new a: ", a)
