0e94218a906e7169c46d3deae945ca39cb5ca6a5
Barengific <75044077+Barengific@users.noreply.github.com>
2022-03-21 20:36:26 +0000

    Update Multiclass_Sequential_Perceptron_LA_LD.py

--- Multiclass_Sequential_Perceptron_LA_LD.py
+++ Multiclass_Sequential_Perceptron_LA_LD.py
@@ -42,17 +42,23 @@ g3 = ''
 
 
 it = 0
-f = False
-for j in range(0,3):
+#increase epoch amount to reach convergence
+for j in range(0,10):
     print("___Epoch___", j+1)            
-    for i in range(0,c):  
+    for i in range(0,c):
+        it = it+1
+        print("[it: ", it)
         print("a1: ", a1)
         print("a2: ", a2)
-        print("a3: ", a3)          
-        it = it+1
+        print("a3: ", a3) 
+        print("y: ", y[:,i])          
         g1 = np.dot(a1.transpose(),y[:,i])
         g2 = np.dot(a2.transpose(),y[:,i])
         g3 = np.dot(a3.transpose(),y[:,i])
+        print("#g1: ", g1)
+        print("#g2: ", g2)
+        print("#g3: ", g3)
+        print("w: ", w_[i]) 
         
         if g1 == g2 == g3:
             temp1 = (lr*y[:,i])
@@ -63,30 +69,30 @@ for j in range(0,3):
             
             continue
         
-        if w_[i] == 1:
+        if (g1 < (g2 or g3)) and ((w_[i] == 1) and (g2 > 0)) or ((w_[i] == 1) and (g3 > 0)):
             temp1 = (lr*y[:,i])
             a1 = np.add(a1, temp1)
-            if g2 > 0:
+            if g2 > g3:
                 temp1 = (lr*y[:,i])*(-1)
                 a2 = np.add(a2, temp1)
             elif g3 > 0:
                 temp1 = (lr*y[:,i])*(-1)
                 a3 = np.add(a3, temp1)
             
-        if w_[i] == 2:
+        if (g2 < (g1 or g3)) and ((w_[i] == 2) and (g1 > 0)) or ((w_[i] == 2) and (g3 > 0)):
             temp1 = (lr*y[:,i])
             a2 = np.add(a2, temp1)
-            if g1 > 0:
+            if g1 > g3:
                 temp1 = (lr*y[:,i])*(-1)
                 a1 = np.add(a1, temp1)
             elif g3 > 0:
                 temp1 = (lr*y[:,i])*(-1)
                 a3 = np.add(a3, temp1)
         
-        if w_[i] == 3:
+        if (g3 < (g2 or g1)) and ((w_[i] == 3) and (g2 > 0)) or ((w_[i] == 3) and (g1 > 0)):
             temp1 = (lr*y[:,i])
             a3 = np.add(a3, temp1)
-            if g1 > 0:
+            if g1 > g2:
                 temp1 = (lr*y[:,i])*(-1)
                 a1 = np.add(a1, temp1)
             elif g2 > 0:
@@ -104,39 +110,3 @@ print("new a1: ", a1)
 print("new a2: ", a2)
 print("new a3: ", a3)
 
-
-
-
-
-
-
-# it = 0
-# f = False
-# for j in range(0,3):
-#     print("___Epoch___", j+1)
-#     if f == False:
-#         for i in range(0,c):
-#             it = it+1
-#             g1 = np.dot(a1.transpose(),y[:,i])
-#             g2 = np.dot(a2.transpose(),y[:,i])
-#             g3 = np.dot(a3.transpose(),y[:,i])
-
-#             if ((g > 0) and (w_[i] == -1)) or  ((g <= 0) and (w_[i] == 1)):
-#                 temp1 = (lr*w_[i])*y[:,i]
-#                 a = np.add(a, temp1)
-#                 print("a: ", a)
-                
-#     for i in range(0,c):
-#         it = it+1
-#         g1 = np.dot(a1.transpose(),y[:,i])
-#         g2 = np.dot(a2.transpose(),y[:,i])
-#         g3 = np.dot(a3.transpose(),y[:,i])
-
-#         if ((g > 0) and (w_[i] == -1)) or  ((g <= 0) and (w_[i] == 1)):
-#             temp1 = (lr*w_[i])*y[:,i]
-#             a = np.add(a, temp1)
-#     print("a: ", a)
-
-   
-# print()    
-# print("new a: ", a)
