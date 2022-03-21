3236a611325194b60d18490c610b5b4a785f46d2
Barengific <75044077+Barengific@users.noreply.github.com>
2022-03-21 20:01:42 +0000

    Update Multiclass_Sequential_Perceptron_LA_LD.py

--- Multiclass_Sequential_Perceptron_LA_LD.py
+++ Multiclass_Sequential_Perceptron_LA_LD.py
@@ -42,35 +42,67 @@ g3 = ''
 
 
 it = 0
+f = False
 for j in range(0,3):
     print("___Epoch___", j+1)            
-    for i in range(0,c):
+    for i in range(0,c):  
+        print("a1: ", a1)
+        print("a2: ", a2)
+        print("a3: ", a3)          
         it = it+1
         g1 = np.dot(a1.transpose(),y[:,i])
         g2 = np.dot(a2.transpose(),y[:,i])
         g3 = np.dot(a3.transpose(),y[:,i])
         
-        if w_ == 1:
-            temp1 = (lr*w_[i])*y[:,i]
+        if g1 == g2 == g3:
+            temp1 = (lr*y[:,i])
             a1 = np.add(a1, temp1)
             
-        if w_ == 2:
-            temp1 = (lr*w_[i])*y[:,i]
-            a2 = np.add(a2, temp1)
-        
-        if w_ == 3:
-            temp1 = (lr*w_[i])*y[:,i]
+            temp1 = (lr*y[:,i])*(-1)
             a3 = np.add(a3, temp1)
             
+            continue
         
-        if ((g > 0) and (w_[i] == -1)) or  ((g <= 0) and (w_[i] == 1)):
-            temp1 = (lr*w_[i])*y[:,i]
-            a = np.add(a, temp1)
-    print("a: ", a)
+        if w_[i] == 1:
+            temp1 = (lr*y[:,i])
+            a1 = np.add(a1, temp1)
+            if g2 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a2 = np.add(a2, temp1)
+            elif g3 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a3 = np.add(a3, temp1)
+            
+        if w_[i] == 2:
+            temp1 = (lr*y[:,i])
+            a2 = np.add(a2, temp1)
+            if g1 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a1 = np.add(a1, temp1)
+            elif g3 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a3 = np.add(a3, temp1)
+        
+        if w_[i] == 3:
+            temp1 = (lr*y[:,i])
+            a3 = np.add(a3, temp1)
+            if g1 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a1 = np.add(a1, temp1)
+            elif g2 > 0:
+                temp1 = (lr*y[:,i])*(-1)
+                a2 = np.add(a2, temp1)
+
+    print("")
+    print("__a1: ", a1)
+    print("__a2: ", a2)
+    print("__a3: ", a3)
 
    
 print()    
-print("new a: ", a)
+print("new a1: ", a1)
+print("new a2: ", a2)
+print("new a3: ", a3)
 
 
 
