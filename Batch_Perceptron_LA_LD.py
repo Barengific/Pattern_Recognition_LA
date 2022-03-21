6904470c83ec42d59c0b0ff65a390ca19c304b44
Barengific <75044077+Barengific@users.noreply.github.com>
2022-03-21 11:46:59 +0000

    Update Batch_Perceptron_LA_LD.py

--- Batch_Perceptron_LA_LD.py
+++ Batch_Perceptron_LA_LD.py
@@ -18,21 +18,34 @@
 
 import numpy as np
 
-a = np.array([[-25],
-              [6],
-              [3]])
+# a = np.array([[-25],
+#               [6],
+#               [3]])
+a = np.array([-25,
+              6,
+              3])
 
 lr = 1
 
 y = np.array([[1,1,-1,-1],
               [1,2,-4,-5],
               [5,5,-1,-1]])
+# misclassified if, g(x) <= 0
 
 #epoch 1
 r,c = y.shape
 
 g = ''
+_x = np.array([[0],[0],[0]])
 for i in range(0,c):
     g = np.dot(a.transpose(),y[:,i])
-    print(g)
+    print("***g: ", g)
+    if g <= 0:
+        _x = np.insert(_x, 1, y[:,i], axis=1)
 
+_r,_c = _x.shape
+for i in range(1,_c):    
+    a = np.add(a, _x[:,i])
+    
+print()    
+print("new a: ", a)
