f707fc6af7c5602c4c9907edfdd5de96b0b2a3fb
Barengific <75044077+Barengific@users.noreply.github.com>
2022-03-21 20:55:16 +0000

    Create Sequential_Widrow-Hoff_LA_LD_Bin.py

---
+++ Sequential_Widrow-Hoff_LA_LD_Bin.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# linear discriminant function,
+# using sequential MULTICLASS perceptron learning algorithm
+# with augmented notation
+# learning is done after each data point is misclassified
+
+# data set
+#  x   class
+# 0,1    1
+# 1,2    1
+# 2,1    1
+# -3,1   -1
+# -2,-1  -1
+# -3,-2  -1
+
+# initial values
+# a = (w_0,w^t) 
+# multi-pla
+# a <- a+lr*(b_k-(a*y_k)y_k - where y_k is a misclassified exemplar
+# a = parameters
+# b = margin vector
+
+import numpy as np
+
+a = np.array([1, 0, 0])
+b = np.array([1, 1, 1, 1, 1, 1])
+
+lr = 0.1
+
+y = np.array([[1,1,1,-1,-1,-1],
+              [0,1,2, 3, 2, 3],
+              [2,2,1,-1, 1, 2]])
+# misclassified if, g(x) and w are not on the same positive or negative spectrum
+# ie. both on the postive or both on negative
+
+w_ = np.array([1,1,1,-1,-1,-1])
+
+r,c = y.shape
+g = ''
+
+it = 0
+#increase epoch amount to reach convergence
+for j in range(0,10):
+    print("___Epoch___", j+1)            
+    for i in range(0,c):
+        it = it+1
+        print("[it: ", it)
+        print("a: ", a)
+        print("y: ", y[:,i])          
+        g = np.dot(a.transpose(),y[:,i])
+        print("#g: ", g)
+        print("w: ", w_[i]) 
+        
+        temp1 = (lr*y[:,i])*(-1)
+        a = np.add(a, temp1)
+        
+    print("")
+    print("__a: ", a)
+   
+print()    
+print("new a: ", a)
+
+
